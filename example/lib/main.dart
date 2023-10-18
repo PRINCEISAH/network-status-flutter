840cf73f3298e82d5a8924ea8649f874d7c1b85f
audu isah <37813957+PRINCEISAH@users.noreply.github.com>
2023-10-18 17:10:42 +0100

    feat: check functions

---
+++ example/lib/main.dart
@@ -0,0 +1,82 @@
+// Dart Packages
+import 'dart:async';
+
+// Flutter Packages
+import 'package:flutter/material.dart';
+
+
+// This Package
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Internet Connection Checker Plus Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Internet Connection Checker Plus Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  InternetStatus? _connectionStatus;
+  late StreamSubscription<InternetStatus> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = InternetConnection().onStatusChange.listen((status) {
+      setState(() {
+        _connectionStatus = status;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Your internet connection status is:',
+            ),
+            Text(
+              _connectionStatus?.toString() ?? 'Unknown',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/internet_connection_checker.dart
@@ -0,0 +1,11 @@
+library internet_connection_checker;
+
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:http/http.dart' as http;
+
+part 'src/check_option.dart';
+part 'src/check_result.dart';
+part 'src/check_internet_status.dart';
+part 'src/connection.dart';
